38bba58410a7117042ccdb00da4231bb554ad15a
Mike Dresser <116307817+michaeldresser81@users.noreply.github.com>
2024-03-13 13:22:17 -0400

    complete

--- lib/dog.py
+++ lib/dog.py
@@ -12,4 +12,28 @@ APPROVED_BREEDS = [
 ]
 
 class Dog:
-    pass
+    def __init__(self, name='Mr Pup', breed='Mastiff'):
+        self.name = name
+        self.breed = breed
+    
+    @property
+    def name(self):
+        return self._name
+    
+    @name.setter
+    def name(self, new_name):
+        if type(new_name) == str and (1 <= len(new_name) <= 25):
+            self._name = new_name
+        else:
+            print('Name must be string between 1 and 25 characters.')
+
+    @property
+    def breed(self):
+        return self._breed
+    
+    @breed.setter
+    def breed(self, new_breed):
+        if not new_breed in APPROVED_BREEDS:
+            print('Breed must be in list of approved breeds.')
+        else:
+            self._breed = new_breed
